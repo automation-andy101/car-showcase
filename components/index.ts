6e1c017ee412dea59bf4f5e973b7be6735934e68
Andy Short <automation.andy101@gmail.com>
2023-09-07 21:54:09 +0100

    Added car detail component with dialog popup

--- components/index.ts
+++ components/index.ts
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 import Footer from './Footer'
 import CustomFilter from './CustomFilter'
 import SearchBar from './SearchBar'
-import SearchManufacturer from './searchManufacturer'
+import SearchManufacturer from './SearchManufacturer'
 import CarCard from './CarCard'
 import CarDetails from './CarDetails'
 

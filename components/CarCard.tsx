6860b7f1847b4a90ad11111fdb5cedce37b0a3ef
Andy Short <automation.andy101@gmail.com>
2023-09-08 08:50:04 +0100

    Added car detail component with dialog popup

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -5,7 +5,7 @@ import Image from "next/image"
 
 import { CarProps } from "@/types";
 import { CarDetails, CustomButton } from ".";
-import { calculateCarRent } from "@/utils";
+import { calculateCarRent, generateCarImageUrl } from "@/utils";
 
 
 interface CarCardProps {
@@ -40,7 +40,8 @@ const CarCard = ({ car }: CarCardProps) => {
             </p>
 
             <div className="relative w-full h-40 my-3 object-contain">
-                <Image src="/hero.png" alt="car model" fill priority className="object-contain" />
+                {/* <Image src="/hero.png" alt="car model" fill priority className="object-contain" /> */}
+                <Image src={generateCarImageUrl(car)} alt="car model" fill priority className="object-contain" />
             </div>
 
             <div className="relative w-full mt-2">
